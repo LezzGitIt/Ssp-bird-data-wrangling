8130be3c0a20d325169732b307b4710dbe47bdf2
Aaron Skinner <skinnerayayron93@gmail.com>
2025-03-07 13:20:36 -0800

    Snapshot of working scripts In 00d: Intersect landcover file with 300m buffers in iterative fashion, eliminating protrusions from landcover file.

--- Scripts/00d_DW_extract_LCs.R
+++ Scripts/00d_DW_extract_LCs.R
@@ -21,11 +21,14 @@
 
 # Load libraries & data --------------------------------------------------------
 pkgs <- c(
-  "terra", "tidyterra", "sf", "tidyverse", "janitor", "cowplot", "maptiles",
-  "xlsx", "readxl", "gridExtra", "ggpubr", "conflicted", "landscapemetrics"
+  "terra", "tidyterra", "sf", "tidyverse", "cowplot", "maptiles",
+  "xlsx", "readxl", "gridExtra", "ggpubr", "conflicted", "janitor"
 )
 
-# Load all packages
+# Install any necessary packages 
+#lapply(pkgs, install.packages)
+
+# Load packages
 lapply(pkgs, library, character.only = TRUE)
 
 # Set basic themes, bring in functions, & data
@@ -33,7 +36,7 @@ ggplot2::theme_set(theme_cowplot())
 conflicts_prefer(dplyr::select)
 conflicts_prefer(dplyr::filter)
 source("/Users/aaronskinner/Library/CloudStorage/OneDrive-UBC/Grad_School/Rcookbook/Themes_funs.R")
-load("Rdata/the_basics_01.09.25.Rdata")
+load("Rdata/the_basics_02.27.25.Rdata")
 
 # Bring in data -----------------------------------------------------------
 #Mathilde shapefiles
@@ -46,15 +49,10 @@ shp.lc.L <- map(.x = files.shp, \(shp)
                   clean_names())
 names(shp.lc.L) <- files.shp
 
-# Buffers from Landcover_digitization.R script
-path <- "Derived_geospatial/shp/"
-Buff_50m <- vect(paste0(path, "Buffers_50m/Buffers_50m.shp"))
-#Buff_300m <- vect(paste0(path, "Buffers_300m/Buffers_300m.shp"))
-
 # Formatting LC dataframes
 shp.lc.L[c(2:4)] <- map(shp.lc.L[c(2:4)], \(polys){
   polys %>% mutate(
-    poly_num = row_number(),
+    #poly_num = row_number(),
     image_date = as.Date(image_date),
     img_date_yr = lubridate::years(image_date),
     trees_perc = as.numeric(trees_perc)
@@ -64,54 +62,7 @@ shp.lc.L[c(2:4)] <- map(shp.lc.L[c(2:4)], \(polys){
 # Export with poly number for Natalia
 #shp.lc.L[[2]] %>% writeVector("../../Mentorship/Digitization_Mathilde/Final_docs/landcovers_middle_final_poly_num/landcovers_middle_final_poly_num.shp", overwrite = TRUE)
 
-# Prob polys: identify and remove--------------------------------------------------
-## There are several polygons that are causing problems executing the script. Identify and remove for now until Mathilde / Natalia can fix 
-## Remove invalid polygons identified in sf
-# For whatever reason sf is more strict than terra -- some polygons that are valid in terra, are not valid in sf
-map(shp.lc.L, ~ table(is.valid(.x))) 
-Tf_l <- map(shp.lc.L, \(shp){
-  sf_shp <- sf::st_as_sf(shp) 
-  sf_shp %>% st_is_valid() %>% 
-    table()
-  sf_shp2 <- sf_shp %>% st_make_valid()
-  TF <- sf_shp2 %>% st_is_valid()
-  return(TF)
-}) 
-valid_polys <- shp.lc.L[[2]][Tf_l[[2]], ] #%>% mutate(poly_num = row_number())
-
-## Relate still causes Topology exception
-#terra::relate(valid_polys, pairs = FALSE, relation = "overlaps")
-
-# Identify the polygons that overlap with the given points 
-prob_coords <- matrix(c(-73.793538544036281, 3.6730780784730706), ncol = 2, byrow = TRUE)
-prob_point <- terra::vect(prob_coords, type = "points", crs = crs(shp.lc.L[[2]]))
-TF <- terra::relate(valid_polys, prob_point, 
-                    pairs = FALSE, relation = "intersects") 
-table(TF[,1])
-Prob_poly_id <- which(TF[,1])
-Prob_poly <- valid_polys %>% filter(poly_num %in% Prob_poly_id)
-
-# Use this polygon to bring in additional surrounding polygons:
-TF <- terra::relate(valid_polys, Prob_poly, 
-                    pairs = FALSE, relation = "intersects") 
-table(TF[,1])
-Prob_poly_ids <- which(TF[,1])
-Prob_polys <- valid_polys %>% filter(poly_num %in% Prob_poly_ids)
-
-# Visualize the problematic point and the surrounding polygons 
-ggplot() +
-  geom_spatvector(data = Prob_polys) +
-  geom_spatvector(data = Prob_poly, fill = "red") +
-  geom_spatvector(data = prob_point, color = "blue", size = 4, alpha = 0.5) + 
-  scale_x_continuous(labels = scales::label_number(accuracy = 0.1)) +  # Round to 1 decimal place
-  labs(x = "Longitude",
-       y = "Latitude") +
-  theme_minimal() 
-
-## REMOVE: Prob_polygon
-valid_polys2 <- valid_polys #%>% filter(!poly_num %in% Prob_poly_id)
-
-# >Buffer -----------------------------------------------------------------
+# Buffer -----------------------------------------------------------------
 ## Format linear features & individual trees
 # Add LC
 shp.lc.L[c(1, 5:7)] <- map2(
@@ -142,6 +93,7 @@ t_lf_agg[[1]] <- t_lf_agg[[1]] %>%
   mutate(trees_perc = 100)
 
 # Erase -------------------------------------------------------------------
+## First erase (live fences) to create LC_holes1
 # After buffering there may still be overlap between individual trees & live fences, but there shouldn't be. Cut out live fences using the individual trees polygons
 trees_lf_buff[2:4] <- map2(t_lf_agg[2:4], t_lf_agg[1], \(lfs, trees){
   terra::erase(lfs, trees)
@@ -149,14 +101,15 @@ trees_lf_buff[2:4] <- map2(t_lf_agg[2:4], t_lf_agg[1], \(lfs, trees){
 
 ## Cut out the buffered individual trees & live fences from the LC polygons
 # Live fences
-Lc_holes1 <- erase(valid_polys2, trees_lf_buff[[2]])
+Lc_holes1 <- erase(shp.lc.L[[2]], trees_lf_buff[[2]])
 
-# NOTE:: Different lengths, certain metadata is being deleted with erase()
+# CHECK lengths:: Has any metadata been deleted with erase()? If so, important to remove the missing IDs
 nrow(Lc_holes1)
 length(Lc_holes1$poly_num)
 
-# Remove the 5 polygons that are lost during erase
-miss_ids <- setdiff(valid_polys2$poly_num, Lc_holes1$poly_num) 
+# >Rm miss_ids ------------------------------------------------------------
+# Remove the polygons that are lost during erase so metadata aligns
+miss_ids <- setdiff(shp.lc.L[[2]]$poly_num, Lc_holes1$poly_num) 
 miss_ids <- setNames(miss_ids, miss_ids)
 Lc_holes2 <- Lc_holes1 %>% filter(!poly_num %in% miss_ids)
 
@@ -164,7 +117,22 @@ Lc_holes2 <- Lc_holes1 %>% filter(!poly_num %in% miss_ids)
 nrow(Lc_holes2)
 length(Lc_holes2$poly_num)
 
-# Erase the individual trees
+# Visualize polygons that are lost in the series of erases
+#if(FALSE){
+miss_id_plots <- imap(miss_ids, \(id, name){
+  miss_poly <- shp.lc.L[[2]] %>% filter(poly_num == id)
+  ggplot() + 
+    geom_spatvector(data = miss_poly) + 
+    labs(title = name) + 
+    theme_min
+})
+if(length(miss_id_plots) < 15){
+  miss_id_plots
+}
+#}
+#--- End >Rm miss_ids
+
+## Second erase (individual trees) to create LC_holes2
 Lc_holes3 <- erase(Lc_holes2, trees_lf_buff[[1]])
 
 # Combine landcovers ------------------------------------------------------
@@ -178,31 +146,32 @@ Lcs_comb2 <- Lcs_comb %>% mutate(lc_typ2 = case_when(
   .default = lc_typ
 ))
 
-
-# Visualize an example buffer
-ggplot() +
-  geom_spatvector(data = Lcs_comb2, aes(fill = lc_typ2), alpha = .4) +
-  #geom_spatvector(data = cropped_lcs, aes(fill = lc_typ2), alpha = .8) +
-  geom_spatvector(data = Buff_50m, alpha = .3) +
-  geom_sf(data = Pc_locs_sf, size = .5) + 
-  coord_sf(xlim = c(-73.683, -73.666), ylim = c(3.323, 3.343))
-
-# Visualize polygons that are lost in the 
+## Visualize an example buffer
 if(FALSE){
-  miss_id_plots <- imap(miss_ids, \(id, name){
-    miss_poly <- valid_polys2 %>% filter(poly_num == id)
-    ggplot() + 
-      geom_spatvector(data = miss_poly) + 
-      labs(title = name) + 
-      theme_min
-  })
-  ggarrange(miss_id_plots[[1]], miss_id_plots[[2]], miss_id_plots[[3]], miss_id_plots[[4]], miss_id_plots[[5]])
+  # Buffers from Landcover_digitization.R script
+  path <- "Derived_geospatial/shp/"
+  Buff_50m <- vect(paste0(path, "Buffers_50m/Buffers_50m.shp"))
+  #Buff_300m <- vect(paste0(path, "Buffers_300m/Buffers_300m.shp"))
+  ggplot() +
+    geom_spatvector(data = Lcs_comb2, aes(fill = lc_typ2), alpha = .4) +
+    #geom_spatvector(data = cropped_lcs, aes(fill = lc_typ2), alpha = .8) +
+    geom_spatvector(data = Buff_50m, alpha = .3) +
+    geom_sf(data = Pc_locs_sf, size = .5) + 
+    coord_sf(xlim = c(-73.683, -73.666), ylim = c(3.323, 3.343))
 }
 
 # Extract LC  -------------------------------------------------------------
 Pc_vect <- Pc_locs_sf %>% vect() %>% 
   project("EPSG:4686")
 Pc_vect_proj <- Pc_vect %>% project("EPSG:3116")
+
+# ALTERNATIVE APPROACH NEW DOESN'T WORK
+if(FALSE){
+  Pc_vect <- Pc_locs_sf %>% 
+    distinct(Ecoregion, Departamento, Id_group, Id_muestreo, geometry) %>%
+    vect() %>% 
+    project("EPSG:4686")
+}
 
 Buffer_rad_nmr <- seq(from = 300, to = 50, by = -50)
 Buffer_rad_nmr <- setNames(Buffer_rad_nmr, Buffer_rad_nmr)
@@ -211,21 +180,135 @@ Buffers <- map(Buffer_rad_nmr, \(rad){
     select(Uniq_db, Ecoregion, Departamento, Id_group, Id_muestreo)
 })
 
-# Intersect each buffer with the landcover object
-cropped_lcs <- Lcs_comb2 %>% terra::intersect(Buffers$`300`) %>%
-  project("EPSG:3116") %>%
-  mutate(poly_num = row_number())
+##  Intersect each buffer with the landcover object
+# NEW APPROACH (FAIL)
+#if(FALSE){
+Buff_300_l <- Buffers$`300` %>% terra::split("Id_muestreo")
+Intersect_lcs <- map(Buff_300_l, \(buff_id){
+  Lcs_comb2 %>% terra::intersect(buff_id) %>% 
+    project("EPSG:3116")
+})
+#}
+
+# OLD APPROACH 
+if(FALSE){
+  Intersect_lcs <- Lcs_comb2 %>% terra::intersect(Buffers$`300`) %>% 
+    project("EPSG:3116") %>%
+    mutate(poly_num = row_number())
+}
+
+# Perf circle - DELETE
+ggplot() + geom_spatvector(data = Intersect_lcs$`G-AD-M-LC_01`)
+
+# Snap vertices -----------------------------------------------------------
+# There are small gaps in the data, which will elevate some landscape metrics (e.g. total edge). Fill in these gaps. 
+Id_groups <- unique(Intersect_lcs$Id_group)
+start <- Sys.time()
+# NOTE:: tidyterra::filter() is not working in map function? Weird
+Snapped_lcs_l <- map(Intersect_lcs, \(Lc_id) {
+  Lc_id %>% snap(tolerance = 5)
+})
+Sys.time() - start 
+
+# Return back to a single SpatVector
+Snapped_lcs <- Snapped_lcs_l %>% vect()
+
+# Troubleshoot -----------------------------------------------------------------
+# Identify problematic polys & Id groups
+dissappear_polys <- c(1559, 1569)
+Prob_id_groups <- c("G-AD-M-EPO3", "U-MB-M-EPO3")
+
+# Select problematic buffers
+Id_epo3 <- Buffers$`300` %>% filter(Id_group %in% Prob_id_groups )
+
+# Visualize problem
+Id_epo3_cropped <- Lcs_comb2 %>% terra::intersect(Id_epo3)
+ggplot() + geom_spatvector(data = Id_epo3_cropped) 
+
+# I confirmed the problem begins with Lc_holes1 object (after first erase), but don't remember how I determined this.
+Lc_holes1 
+
+## Intersect epo3 buffer with vp2
+# NOTE:: It is not the act of intersect that is removing the polygons in question
+Cropped_lcs_vp2 <- shp.lc.L[[2]] %>% terra::intersect(Id_epo3) %>%
+  project("EPSG:3116")
+
+EPO3_07 <- Cropped_lcs_vp2 %>% filter(Id_muestreo == "G-AD-M-EPO3_07")
+
+ggplot() + geom_spatvector(data = Cropped_lcs_vp2, alpha = .7) + 
+  geom_spatvector(data = EPO3_07, aes(fill = lc_typ), alpha = .6)
+
+## Highlight polygons that are disappearing w/ erase in blue. There are livefences likely causing at least some of the problems. Please redraw these livefences as polygons (use imagery to estimate the appropriate width & shape)
+miss_polys_vp2 <- shp.lc.L[[2]] %>% filter(poly_num %in% dissappear_polys)
+ggplot() + 
+  geom_spatvector(data = miss_polys_vp2, fill = "pink", alpha = .7) +
+  geom_spatvector(data = miss_polys_vp2, 
+                  color = "blue", alpha = 1, linewidth = 3) +
+  geom_spatvector(data = Id_epo3_cropped, aes(fill = lc_typ2), alpha = .3) 
 
 # Export ------------------------------------------------------------------
-
 ## Export cropped landcover shapefile - The polygons that mathilde digitized that have been processed in this R script
-cropped_lcs %>% 
-  terra::writeVector("Derived_geospatial/shp/Cropped_lcs/Cropped_lcs.shp")
+Snapped_lcs %>% 
+  terra::writeVector("Derived_geospatial/shp/Snapped_lcs.gpkg") #overwrite = TRUE
 
 # Extras -----------------------------------------------------------------
-# >Working ----------------------------------------------------------------
+# >Overlapping polys ------------------------------------------------------
+# Create a safe version of the `relate()` function with `possibly`
+safe_relate <- possibly(
+  ~relate(.x, relation = "overlaps", pairs = TRUE),
+  otherwise = NULL,
+  quiet = FALSE
+)
 
-# >Invalid polygons  -------------------------------------------------------
+# Map over the input with safe_relate function
+overlap_matrix <- map(
+  shp.lc.L[[2]], safe_relate, .progress = TRUE
+)
+
+# Sort each row and remove duplicates
+overlap_indices <- map(overlap_matrix, \(om){
+  unique(t(apply(om, 1, sort))) %>% as.data.frame()
+})
+
+## Generate overlap 'area' vector & plots for Natalia
+# For 'middle_final' shapefile
+area_middle <- as.numeric()
+plots_middle <- list()
+for (i in 1:nrow(overlap_indices$landcovers_middle_final)) { #
+  print(i)
+  index <- overlap_indices$landcovers_middle_final %>% slice(i)
+  overlapping <- shp.lc.L$landcovers_middle_final[c(index[, 1], index[, 2])]
+  intersection <- terra::intersect(overlapping[1, ], overlapping[2, ])
+  if (!is.empty(intersection)) {
+    area_middle[i] <- round(expanse(intersection, unit = "m"), 2)
+  } else {
+    area_middle[i] <- 0
+  }
+  if (area_middle[i] > 10) {
+    p <- ggplot() +
+      geom_spatvector(
+        data = overlapping,
+        fill = c("red", "green"), alpha = .3
+      ) +
+      geom_spatvector(data = intersection, fill = "black") +
+      labs(
+        title = paste0("Polygons ", index[, 1], ", ", index[, 2]),
+        subtitle = paste0("Overlap area = ", area_middle[i], " meters sq")
+      ) +
+      theme_min
+    # Save plots to list
+    plots_middle[[length(plots_middle) + 1]] <- p
+  }
+}
+sort(area_middle)
+
+# Confirm # of plots
+length(plots_middle) # Only plots > 150 meters squared
+# Plots middle
+ggarrange(plots_middle[[1]], plots_middle[[2]], plots_middle[[3]], plots_middle[[4]], plots_middle[[5]], ncol = 2, nrow = 3)
+
+# >Problematic polygons ---------------------------------------------------
+# >>Invalid ---------------------------------------------------------------
 # NOTE:: There are 45 polygons that are invalid by sf standards. These don't seem to influence the workflow at present but important to keep in mind if things start to go wrong. 
 # See below for plotting of these polygons
 if(FALSE){
@@ -249,6 +332,54 @@ map(polygon_list, ~ ggplot() +
       labs(title = paste("Polygon", as.character(.x$poly_num))) +
       theme_minimal())
 ##
+
+# >>Topology exception-------------------------------------------------
+## There were several polygons that were causing problems executing the script. Identify and remove for now until Mathilde / Natalia can fix 
+
+## At present, the relate function works! (no Topology exception), thus the rest of this code is unnecessary 
+terra::relate(shp.lc.L[[2]], pairs = FALSE, relation = "overlaps")
+
+## Remove invalid polygons identified in sf
+# For whatever reason sf is more strict than terra -- some polygons that are valid in terra, are not valid in sf
+map(shp.lc.L, ~ table(is.valid(.x))) 
+Tf_l <- map(shp.lc.L, \(shp){
+  sf_shp <- sf::st_as_sf(shp) 
+  sf_shp %>% st_is_valid() %>% 
+    table()
+  sf_shp2 <- sf_shp %>% st_make_valid()
+  TF <- sf_shp2 %>% st_is_valid()
+  return(TF)
+}) 
+valid_polys <- shp.lc.L[[2]][Tf_l[[2]], ] #%>% mutate(poly_num = row_number())
+
+# Identify the polygons that overlap with the given points 
+prob_coords <- matrix(c(-73.793538544036281, 3.6730780784730706), ncol = 2, byrow = TRUE)
+prob_point <- terra::vect(prob_coords, type = "points", crs = crs(shp.lc.L[[2]]))
+TF <- terra::relate(valid_polys, prob_point, 
+                    pairs = FALSE, relation = "intersects") 
+table(TF[,1])
+Prob_poly_id <- which(TF[,1])
+Prob_poly <- valid_polys %>% filter(poly_num %in% Prob_poly_id)
+
+# Use this polygon to bring in additional surrounding polygons:
+TF <- terra::relate(valid_polys, Prob_poly, 
+                    pairs = FALSE, relation = "intersects") 
+table(TF[,1])
+Prob_poly_ids <- which(TF[,1])
+Prob_polys <- valid_polys %>% filter(poly_num %in% Prob_poly_ids)
+
+# Visualize the problematic point and the surrounding polygons 
+ggplot() +
+  geom_spatvector(data = Prob_polys) +
+  geom_spatvector(data = Prob_poly, fill = "red") +
+  geom_spatvector(data = prob_point, color = "blue", size = 4, alpha = 0.5) + 
+  scale_x_continuous(labels = scales::label_number(accuracy = 0.1)) +  # Round to 1 decimal place
+  labs(x = "Longitude",
+       y = "Latitude") +
+  theme_minimal() 
+
+## REMOVE: Prob_polygon
+valid_polys2 <- valid_polys #%>% filter(!poly_num %in% Prob_poly_id)
 
 # >Subsetting -------------------------------------------------------------
 # Large rasters, can be helpful to subset to more manageable size
--- Scripts/00e_DW_Lsm.R
+++ Scripts/00e_DW_Lsm.R
@@ -17,27 +17,31 @@ conflicts_prefer(dplyr::filter)
 source("/Users/aaronskinner/Library/CloudStorage/OneDrive-UBC/Grad_School/Rcookbook/Themes_funs.R")
 
 # Load data
-load("Rdata/the_basics_01.09.25.Rdata")
-Cropped_lcs <- vect("Derived_geospatial/shp/Cropped_lcs/Cropped_lcs.shp")
+load("Rdata/the_basics_02.27.25.Rdata")
+Snapped_lcs <- vect("Derived_geospatial/shp/Snapped_lcs.gpkg")
 
 # Rasterize --------------------------------------------------------------
-
 # Rasterize ensuring landcover is the value of each cell
-rast <- cropped_lcs %>%  #Lcs_sub
+rast <- Snapped_lcs %>%  #Lcs_sub
   rast(resolution = 1)
 
 # Generate a list with each Id_muestreo 
-Lcs_id_muestreo <- cropped_lcs %>% terra::split("Id_muestreo")
+Lcs_id_muestreo <- Snapped_lcs %>% terra::split("Id_muestreo")
 
 # Create extent with desired resolution
 rast_l <- map(Lcs_id_muestreo, \(polys_group){
-  polys_group %>% rast(resolution = 2)
+  polys_group %>% rast(resolution = 1)
 })
 
 # Rasterize polygons -- very quick 
 Lc_rast_l <- map2(Lcs_id_muestreo, rast_l, \(polys_id, rast) {
   Lc_rast <- terra::rasterize(polys_id, rast, field = "lc_typ2") # Lcs_sub
 })
+
+# Mask raster
+#Lc_rast_crop <- map2(Lcs_id_muestreo, Lc_rast_l, \(polys_id, rast) {
+ # terra::crop(rast, polys_id, mask = TRUE, touches = TRUE, snap = "out")
+#})
 
 # join_lc_class ---------------------------------------------------------------
 
@@ -77,7 +81,6 @@ join_lc_class <- map2(Lc_rast_l, uniq_classes, \(rast, class) {
   mutate(lc_typ2 = ifelse(is.na(lc_typ2), "empty", lc_typ2)) 
 
 # Visualize rasters -------------------------------------------------------
-
 ## Visualize buffers if helpful (e.g. polygons with raster cells == NA)
 # NOTE:: All Id_groups will show NAs due to the raster cells OUTSIDE of landcovers
 ggplot() +
@@ -91,6 +94,10 @@ plots_u4 <- imap(Lc_rast_l[not4_i[1:3]], \(rast, group){
 })
 #plots_u4
 
+# Or any specific problems 
+# G-AD-M-LC_01 has several bumps sticking out, several internal holes as well
+ggplot() + geom_spatraster(data = Lc_rast_l[["G-AD-M-LC_01"]])
+
 # Visualize with satellite imagery 
 sat_bg <- get_tiles(Lc_rast_l[[1]], crop = TRUE, provider = "Esri.WorldImagery")
 plot_tiles(sat_bg) 
@@ -102,13 +109,18 @@ plot(Lc_rast_l[[1]], add = TRUE, alpha = .5)
 #click(id = TRUE)
 
 # Calc metrics ------------------------------------------------------------
-Pc_vect <- Pc_locs_sf %>% vect() %>% 
+## Calculate lsm using Landscapemetrics package
+
+# Generate objects going into scale_sample() function
+Pc_vect <- Pc_locs_sf %>% 
+  vect() %>% 
   project("EPSG:4686")
 Pc_vect_proj <- Pc_vect %>% project("EPSG:3116")
-
-# Calculate lsm using Landscapemetrics package
-Pc_cents <- Pc_vect_proj %>% filter(!Id_muestreo %in% paste0("OQ_0", 7:9)) %>%
+Pc_cents <- Pc_vect_proj %>%
   terra::split("Id_muestreo")
+
+Buffer_rad_nmr <- seq(from = 300, to = 50, by = -50)
+Buffer_rad_nmr <- setNames(Buffer_rad_nmr, Buffer_rad_nmr)
 
 # Generate lsm for all items in these lists
 start <- Sys.time()
@@ -129,36 +141,45 @@ Lsm_df <- Lsm_l %>% list_rbind(names_to = "Id_muestreo") %>%
   select(-c(plot_id, id, layer)) %>% 
   left_join(join_lc_class)
 
-# Should be no NAs if merge worked appropriately
+# Should be no NAs if merge worked appropriately.
 Lsm_df %>% filter(is.na(lc_typ2))
+
+# On 2.28.25 there started being a problematic class 4 here in 3 point counts, not sure what that's about. It seems like all others have class starting at 0, and this starts at 1
+join_lc_class %>% filter(Id_muestreo == "C-MB-VC-LCH_04")
+Lsm_df %>% filter(Id_muestreo == "C-MB-VC-LCH_04") %>% 
+  pull(class)
 
 # Inspect Lsm_df ----------------------------------------------------------
 
 # Instead of recalculating (slow), load in lsm file
 #Lsm_df <- read_xlsx("Derived/Excels/Lsm/Lsm_df_02.08.25.xlsx")
 
-## NOTE:: There are 'percentage_inside' over 100 and under 90. Generate 1 row per Id_muestreo to handle more easily 
-To_digitize <- Lsm_df %>% filter(percentage_inside < 100) %>% #> 100
+## NOTE:: There are 'percentage_inside' (pi) over 100 and under 98. Generate 1 row per Id_muestreo to handle more easily 
+Low_pi <- Lsm_df %>% filter(percentage_inside < 100) %>% #> 100
   summarize(percent_inside = min(percentage_inside), .by = Id_muestreo) %>%
   mutate(sum_fun = "min") %>%
-  arrange(percent_inside) #%>%
-#filter(str_detect(Id_muestreo, "\\(1\\)|OQ")) 
-To_digitize
+  arrange(percent_inside)# %>%
+  #filter(str_detect(Id_muestreo, "\\(1\\)|OQ")) 
+Low_pi
 
-over100 <- Lsm_df %>% filter(percentage_inside >= 104.7) %>% 
+High_pi <- Lsm_df %>% filter(percentage_inside >= 100) %>% 
   summarize(percent_inside = max(percentage_inside), .by = Id_muestreo) %>%
   mutate(sum_fun = "max") %>% 
   arrange(desc(percent_inside))
-over100 
+High_pi
 
-# Create dataframe of ids that are problematic due to percentage inside (pi)
-prob_pi_ids <- rbind(To_digitize, over104) %>% 
+# Create dataframe of ids that are problematic due to percentage inside
+prob_pi_ids <- rbind(Low_pi, High_pi) %>% 
   mutate(across(where(is_double), ~ round(.x, 2)))
+
+# Subset the 20 ids with the lowest & highest percent inside
+prob_ids_min_max <- prob_pi_ids %>% slice(1:10, .by = sum_fun)
 
 # Plot problematic pc buffers ------------------------------------------------
 
-# Plot, using pmap to iterate over dataframe in rowwise fashion
-prob_pi_plots <- pmap(prob_pi_ids[,1:2], \(Id_muestreo, percent_inside) {
+## Plot, using pmap to iterate over dataframe in rowwise fashion
+# Can use prob_pi_ids if that is better, but the highest percentage_inside values aren't very informative visually
+prob_pi_plots <- pmap(prob_ids_min_max[,1:2], \(Id_muestreo, percent_inside) {
   ggplot() +
     geom_spatraster(data = Lc_rast_l[[Id_muestreo]]) + 
     labs(subtitle = Id_muestreo, 
@@ -167,13 +188,20 @@ prob_pi_plots <- pmap(prob_pi_ids[,1:2], \(Id_muestreo, percent_inside) {
     theme(legend.position = "none")
 })
 # Visualize example
-prob_pi_plots[[2]]
+# prob_pi_plots
 
 ## Print PDF file with 9 plots per page
-pdf("Figures/Prob_polys/Polygons_percent_inside.pdf")
-print(marrangeGrob(grobs = prob_pi_plots, ncol = 3, nrow = 3, 
-                   layout_matrix = matrix(1:9, 3, 3, TRUE)))
-dev.off()
+if(FALSE){
+  pdf("Figures/Prob_polys/Polygons_percent_inside.pdf")
+  print(marrangeGrob(grobs = prob_pi_plots, ncol = 3, nrow = 3, 
+                     layout_matrix = matrix(1:9, 3, 3, TRUE)))
+  dev.off()
+}
+
+# There are several missing polygons in these Id_groups
+Prob_id_groups <- Snapped_lcs %>% 
+  filter(Id_group %in% c("G-AD-M-EPO3", "U-MB-M-EPO3"))  
+ggplot() + geom_spatvector(data = Prob_id_groups, aes(fill = lc_typ2)) 
 
 # Correlations ------------------------------------------------------------
 # Not sure exactly what this is doing, may be easier to just use cor()
@@ -189,16 +217,24 @@ corr_df <- corr_l %>% list_rbind(names_to = "Id_muestreo") %>%
 
 # Save and export ---------------------------------------------------------
 # Export centroids of point counts still to be digitized for Natalia
-To_digitize %>% left_join(Pc_locs_sf) %>%
-  slice_max(Buffer_rad) %>%
+Low_pi %>% left_join(Pc_locs_sf) %>%
   filter(Id_muestreo != "OQ_Practica") #%>% 
 #st_write("Derived_geospatial/shp/To_digitize_natalia/To_digitize.shp")
 
+# Export 
+prob_pi_ids %>% as.data.frame() #%>%
+  #write.xlsx(file = paste0("Derived/Excels/Lsm/Polys_percent_inside.xlsx"), 
+  #showNA = FALSE, row.names = FALSE
+#)
+
 # Export Excel of lsm
-Lsm_df %>% as.data.frame() %>%
-  #if(FALSE){
-  write.xlsx(
-    file = paste0("Derived/Excels/Lsm/Lsm_df_", format(Sys.Date(), "%m.%d.%y"), ".xlsx"), 
-    showNA = FALSE, row.names = FALSE
+Lsm_df_exp <- Lsm_df %>% as.data.frame() 
+Lsm_df_exp %>%
+  if(FALSE){
+    write.xlsx(
+    file = paste0("Derived/Excels/Lsm/Lsm_df_", format(Sys.Date(), "%m.%d.%y"), ".xlsx"), showNA = FALSE, row.names = FALSE
   )
-#}
+  }
+
+stop()
+warnings() # 50+ warnings: Double values will be converted to integer.
