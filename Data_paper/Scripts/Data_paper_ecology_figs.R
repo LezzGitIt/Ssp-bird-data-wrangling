569c36b1d2082eb036ec1201a8f505a57119278c
Aaron Skinner <skinnerayayron93@gmail.com>
2025-09-24 12:16:33 -0700

    Code to create the metadata tables (including extract_metadata function) and plot of breakdown of point counts and point count repetitions by farm.

--- Data_paper/Scripts/Data_paper_ecology_figs.R
+++ Data_paper/Scripts/Data_paper_ecology_figs.R
@@ -1,23 +1,51 @@
 ## Data paper Ecology figures ## 
 
-## Load data & libraries
+# Load libraries -------------------------------------------------
 # Libraries
 library(readxl)
+library(xlsx)
 library(tidyverse)
 library(sf)
+library(hms)
 library(ggpubr)
 library(cowplot)
 library(ggrepel)
 library(rnaturalearthdata)
 library(rnaturalearth)
+library(geodata)
 library(smoothr)
+library(terra)
+library(tidyterra)
 library(ggspatial)
+library(conflicted)
 ggplot2::theme_set(theme_cowplot())
+conflicts_prefer(dplyr::lag)
+conflicts_prefer(hms::hms)
+conflicts_prefer(dplyr::filter)
 
+# Load data  --------------------------------------------------------------
 load("Rdata/NE_layers_Colombia.Rdata")
-load("Rdata/the_basics_07.18.25.Rdata")
+#load("Rdata/the_basics_07.18.25.Rdata")
+
+Pc_locs_sf <- st_read("Derived_geospatial/shp/Pc_locs.gpkg")
+
+# Load in raw abundance data
+Bird_pcs <- read_csv("Data_paper/DataS1/Bird_pcs.csv")
+Birds_analysis <- read_csv("Data_paper/DataS1/Birds_analysis.csv")
+Taxonomy <- read_csv(file = "Data_paper/DataS1/Taxonomy.csv")
+Fn_traits <- read_csv(file = "Data_paper/DataS1/Functional_traits.csv") %>% 
+  select(-Match_type)
+Site_covs <- read_csv(file = "Data_paper/DataS1/Site_covs.csv")
+Event_covs <- read_csv(file = "Data_paper/DataS1/Event_covs.csv")
 
 source("/Users/aaronskinner/Library/CloudStorage/OneDrive-UBC/Grad_School/Rcookbook/Themes_funs.R")
+
+Site_covs %>% left_join(Event_covs) %>%
+  filter(Uniq_db == "Gaica mbd") %>%
+  distinct(Id_gcs, Id_muestreo) %>%
+  count(Id_gcs) %>% 
+  arrange(n)
+  #arrange(desc(Pc_length))
 
 # Fig1: Sampling map ------------------------------------------------------
 ### Create map showing point count locations on informative background (elevation)
@@ -42,11 +70,11 @@ Col_alt_map <- ggplot() +
 # >Precip background -------------------------------------------------------
 
 Prec_col <- worldclim_country(country = "COL", var = "prec", path = Envi_path)
-tot.prec <- sum(Prec_col)
+Tot_prec <- sum(Prec_col)
 
 ## Colombia precip map 
 Col_prec_map <- ggplot() + 
-  geom_spatraster(data = tot.prec) + 
+  geom_spatraster(data = Tot_prec) + 
   labs(
     x = "Longitude",
     y = "Latitude"
@@ -70,7 +98,8 @@ rivers_co2 <- rivers_co %>%
 # >Point formatting -------------------------------------------------------
 ## Point counts within 0.25 degree grid cells 
 # With ~500 point counts in concentrated regions there is too much overlap to clearly visualize what is going on. Instead, I calculate the number of point counts within .25 degrees cells and return the rounded coordinates for plotting
-Pc_locs_round <- Pc_locs %>%
+Pc_locs_round <- Pc_locs_sf %>%
+  st_drop_geometry() %>%
   mutate(Latitud_rd = mround(Latitud, .25), 
          Longitud_rd = mround(Longitud, .25)) %>%
   count(Uniq_db, Ecoregion, Latitud_rd, Longitud_rd, sort = T) %>%
@@ -170,12 +199,12 @@ explore <- FALSE
 if(explore){
   # Formatting
   Rio_cesar_pts <- Pc_locs_sf %>% filter(Ecoregion == "Rio cesar") 
-  Envi_rc <- Rio_cesar_pts %>% left_join(Envi_df2)
+  Envi_rc <- Rio_cesar_pts %>% left_join(Site_covs)
   
   # Plot zoomed in 
   bbox_rc <- st_bbox(Rio_cesar_pts)
   Col_alt_map + geom_sf(data = Envi_rc,
-                        aes(color = Tot.prec)) + 
+                        aes(color = Tot_prec)) + 
     coord_sf(
       xlim = c(bbox_rc[1], bbox_rc[3]), ylim = c(bbox_rc[2], bbox_rc[4])
     )
@@ -228,7 +257,9 @@ ggsave("Data_paper/Figures/Map_sampling/South_america.png", bg = "white", dpi = 
 # Boxplots showing the temporal distribution of sampling in each ecoregion 
 
 # Create the 'Grp_spat' variable that shows which point counts are surveyed at the same spatial location, which is especially important for Meta
-Meta_PCs_related <- Pc_date8 %>%
+Es_covs <- Event_covs %>% left_join(Site_covs)
+
+Meta_PCs_related <- Es_covs %>%
   filter(Departamento == "Meta" & Uniq_db == "Gaica mbd") %>%
   distinct(Id_muestreo, Ano) %>% # head()
   group_by(Id_muestreo) %>%
@@ -238,7 +269,7 @@ Meta_PCs_related <- Pc_date8 %>%
     Year1 == 2016 & Year2 == 2017 ~ "G1617" # GAICA 2016-2017 is one group
   ))
 
-Pc_date9 <- Pc_date8 %>%
+Pc_date9 <- Es_covs %>%
   left_join(Meta_PCs_related[, c("Id_muestreo", "Grp_spat")],
             by = "Id_muestreo"
   ) %>%
@@ -292,14 +323,14 @@ ggsave("Data_paper/Figures/02_Pc_month_year_day_ecoregion.png", bg = "white", wi
 # Fig3: Environmental vars histogram --------------------------------------
 # Boxplots for Elevation, temp, & precipitation
 p <- list()
-var_names <- c("Elev", "Avg_temp", "Tot.prec")
+var_names <- c("Elev", "Avg_temp", "Tot_prec")
 ylab <- c("meters", "Celsius", "millimeters")
 title <- c("Elevation", "Temperature", "Precipitation")
 ecoreg_labs <- c("Bajo \nMagdalena", "Boyaca \nSantander", "Cafetera", "Piedemonte", "Rio Cesar")
 
 for (i in c(1:3)) {
   print(i)
-  p[[i]] <- Envi_df2 %>%
+  p[[i]] <- Site_covs %>%
     group_by(Ecoregion) %>%
     ggplot(aes(
       x = fct_reorder(Ecoregion, Elev, .fun = median),
@@ -321,10 +352,10 @@ ggsave("Data_paper/Figures/Envi_vars.png", bg = "white", width = 10)
 
 
 # Fig4a: Rainfall density plota -------------------------------------------
-var_names <- c("Elev", "Avg_temp", "Tot.prec")
+var_names <- c("Elev", "Avg_temp", "Tot_prec")
 
 # Correlation matrix -- Notice temp & elevation perfectly inverse correlated
-Envi_df2 %>%
+Site_covs %>%
   select(all_of(var_names)) %>%
   cor() %>%
   data.frame() %>%
@@ -332,7 +363,8 @@ Envi_df2 %>%
 
 # Rainfall seasonality plot
 # For rainfall seasonality plot for determining ideal repeat survey timing for dynamic occupancy models, best to exclude Distancia & CIPAV b/c they only have 1 repeat survey 
-Prec_df2 <- Prec_df %>% filter(Uniq_db != "GAICA Distancia" & Uniq_db != "CIPAV MBD")
+Prec_df2 <- Prec_df %>% 
+  filter(Uniq_db != "GAICA Distancia" & Uniq_db != "CIPAV MBD")
 table(Prec_df2$Uniq_db)
 
 Calc_mean_prec <- function(df, group_variable){
@@ -349,16 +381,18 @@ Prec_ecor <- Calc_mean_prec(df = Prec_df2, group_variable = Ecoregion)
 Prec_depts <- Calc_mean_prec(df = Prec_df2, group_variable = Departamento)
 
 ## Plot precip for all ecoregions using smoothed GAM
-ggplot(data = Prec_ecor) +
-  stat_smooth(method = "gam", se = F, 
-              aes(x = Mes, y = Prec, color = Ecoregion)) +
+# cc = cyclic cubic regression spline - Use because the function value at month 12 is constrained to join smoothly back to month 1.
+# k = the basis dimension, i.e. the maximal degrees of freedom. This allows the smoother to be as wiggly as one wiggle per month.
+ggplot(Prec_ecor, aes(x = Mes, y = Prec, color = Ecoregion)) +
+  stat_smooth(method = "gam", formula = y ~ s(x, bs = "cc", k = 12), se = FALSE) +
   scale_x_continuous(breaks = c(0, 2, 4, 6, 8, 10, 12)) +
   labs(x = "Month", y = "Precipitation (mm)")
 ggsave("Data_paper/Figures/Rainfall/Prec_smoothed.png", bg = "white")
 
 # Fig4b: Precipitation with sampling dates ---------------------------------
 # PCs_prec are the points that go on the rainfall seasonality plot
-Pcs_prec <- Pc_date8 %>% distinct(Uniq_db, Ecoregion, Ano, Mes) %>%
+Pcs_prec <- Es_covs %>% 
+  distinct(Uniq_db, Ecoregion, Ano, Mes) %>%
   left_join(Prec_ecor, by = c("Ecoregion", "Mes")) %>%
   arrange(Ecoregion, Ano, Mes) %>%
   mutate(min = Mes - 2, max = Mes + 2, .by = c(Uniq_db, Ecoregion, Ano)) %>% 
@@ -427,24 +461,152 @@ ggsave("Data_paper/Figures/Rainfall/Prec_sampling_faceted.png", bg = "white")
 
 stop()
 
+# Supplementary figs ------------------------------------------------------
+## Plot showing numer of point counts per farm, the number of farms each data collector surveyed, and the average number of times each point count was repeated within a season (< 80 days)
+
+# Number of pc per farm
+Farm_counts <- Event_covs %>% 
+  left_join(Site_covs) %>%
+  distinct(Id_gcs, Uniq_db, Id_muestreo) %>%
+  count(Id_gcs, Uniq_db) %>%
+  count(Uniq_db, name = "N_farms")
+
+Db_summ <- Event_covs %>% 
+  mutate(Max_rep_season = max(Rep_season), 
+         .by = Id_muestreo) %>%
+  summarize(Mean_rep_season = mean(Max_rep_season),
+            .by = Uniq_db) %>% 
+  mutate(Mean_rep_season = as.factor(round(Mean_rep_season, 0))) %>% 
+  full_join(Farm_counts)
+
+# Number of point counts per farm and database
+Num_pcs_farm_db <- Event_covs %>% 
+  left_join(Site_covs) %>%
+  distinct(Id_gcs, Uniq_db, Id_muestreo) %>%
+  count(Id_gcs, Uniq_db, sort = T)
+## Generate labels for plot, where eaach label is the number of farms surveyed
+# Adjust the location of the label for UniLlanos & UBC 
+x_loc <- summarize(Num_pcs_farm_db, x_loc = max(n) + 1, .by = Uniq_db) %>% 
+  arrange(desc(x_loc)) %>%
+  mutate(x_loc = x_loc - c(rep(7, 2), rep(0,4)))
+label_data <- Farm_counts %>% 
+  left_join(x_loc)
+
+# Plot
+Num_pcs_farm_db %>% 
+  full_join(Db_summ) %>%
+  ggplot(aes(x = n, y = Uniq_db, color = Mean_rep_season)) + 
+  geom_boxplot(outliers = FALSE) + 
+  geom_jitter(alpha = .4, width = 0.01) + 
+  labs(x = "Point counts per farm", 
+       y = "Data set",
+       color = "Repeat surveys \nper point count") + 
+  theme(legend.position = "top") +
+  geom_text(
+    data = label_data,
+    aes(x = x_loc,
+        y = Uniq_db,
+        label = paste("N =", N_farms)),
+    inherit.aes = FALSE,
+    hjust = 0
+  ) 
+#quants <- quantile(Pc_per_farm$n, probs = c(0, .1, .9, 1))
+
+#ggsave("Data_paper/Figures/Pc_per_farm_db.png", bg = "white")
 
 # Data sets ---------------------------------------------------------------
-# >Format -----------------------------------------------------------------
-ten_min_db <- c("Gaica mbd", "Ubc gaica mbd", "Ubc mbd", "Unillanos mbd") 
-Birds_analysis %>%
-  arrange(Ano_grp, Id_group, Id_muestreo, Fecha, Pc_start) %>%
-  relocate(c(Nombre_institucion, Id_gcs), .before = Id_group) %>% 
-  relocate(c(Rep, Count), .before = Orden) %>% 
-  select(-Nombre_ayerbe_) %>% 
-  # If NOT gaica distancia or cipav data, set all point count lengths to 10 minutes to avoid confusion
-  mutate(Pc_length = if_else(
-    Uniq_db %in% ten_min_db, hms(minutes = 10), Pc_length
-  ))
+# >Metadata tbls -----------------------------------------------------------
+## Custom function to extract the metadata for a given dataset 
+extract_metadata <- function(df){
+  map_dfr(names(df), function(col_name) {
+    col_data <- df[[col_name]]
+    if (inherits(col_data, "hms")) {
+      low_val <- min(as.character(col_data, na.rm = TRUE)) 
+      high_val <- max(as.character(col_data, na.rm = TRUE))
+    } else {
+      low_val <- as.character(min(col_data, na.rm = TRUE))
+      high_val <- as.character(max(col_data, na.rm = TRUE))
+    }
+    tibble(
+      Field_name = col_name,
+      Data_type = class(col_data)[1],
+      Low_range = low_val,
+      High_range = high_val
+    )
+  })
+}
 
+### Create metadata tbls
+## Primary abundance file 
+Bird_abu_defs <- c("Unique ID of each point count. Using the first initials of each word and separating with hyphens, the format is: Data collector --  Research question -- Department -- Farm _ Point count number", "Point count ID irrespective of the data collector", "Date", "Start time of point count", "Species name according to Fernando Ayerbe's 2018 field guide (AOS-SACC)", "The distance to the bird from the observer", "Whether species was identified in a recording", "Number of individuals observed")
+Bird_abu_meta <- Bird_pcs %>% 
+  extract_metadata() %>% 
+  mutate(Definition = Bird_abu_defs) %>% 
+  relocate(Definition, .after = Field_name)
 
-# >Export -----------------------------------------------------------------
+## Site covariates
+# DELETE Site_covs_defs <- c("Point count ID irrespective of the data collector", "Ecoregion where the survey was conducted", "Department where the survey was conducted", "Elevation in meters (90 meter resolution)", "Average temperature in °C", "Total precipitation in millimeters", "Predominant habitat within the 50 meter point count radius")
+Site_covs_meta <- extract_metadata(Site_covs)
 
-# Export as .csv
+## Taxonomy file
+Taxonomy_meta <- extract_metadata(Taxonomy)
+
+# Definitions for taxonomy 
+Name_equivalents <- tibble(Initials = c("gbif", "ayerbe", "bl", "bt", "eb"), 
+                           Full_name = c("the Global Biodiversity Information Facility", "Ayerbe-Quiñones (2018)", "bl" = "BirdLife", "BirdTree", "eBird"))
+Tax_lvl <- str_split_i(Taxonomy_meta[1:7,]$Field_name, pattern = "_", i = 1)
+Source <- str_split_i(Taxonomy_meta[1:7,]$Field_name, pattern = "_", i = 2)
+pattern_vector <- setNames(Name_equivalents$Full_name, Name_equivalents$Initials)
+Full_names <- str_replace_all(Source, pattern_vector)
+
+# Define Match_type and Autoria_gbif 
+Match_type_def <- "Taxonomic match level:
+\n --1BL to 1BT: Species equivalent between BirdLife & BirdTree
+\n --Many BL to 1BT: Multiple BirdLife species classified as a single BirdTree species
+\n --1BL to many BT: Multiple BirdTree species classified as a single BirdLife species
+\n --N/D: If species was not found in either BirdLife or BirdTree taxonomies"
+Autoria_gbif_def <- "Taxonomic authority according to the Global Biodiversity Information Facility"
+Defs_match_autoria <- Taxonomy_meta[8:9,] %>% 
+  mutate(Definition = c(Match_type_def, Autoria_gbif_def)) 
+
+# Paste it together to create Definition column and final formatting
+Taxonomy_meta2 <- Taxonomy_meta[1:7,] %>% 
+  mutate(Definition = paste(Tax_lvl, "according to", Full_names)) %>% 
+  bind_rows(Defs_match_autoria)
+
+## Functional traits
+Fn_traits_meta <- Fn_traits %>% extract_metadata() 
+
+# Create definitions
+Definitions_ft_vec <- c("Species according to Ayerbe-Quiñones (2018)", 
+                        "Taxonomy used to match functional trait, where: BL = BirdLife, BT = BirdTree, eB = eBird", 
+                        rep("See Tobias et al (2022)", 24),
+                        "Elevational range specific to Colombia",
+                        "Source from which Elev_range was extracted")
+Definitions_ft_join <- tibble(Field_name = Fn_traits_meta$Field_name, 
+                         Definition = Definitions_ft_vec)
+Fn_traits_meta2 <- Definitions_ft_join %>% left_join(Fn_traits_meta)
+
+# Event covariates 
+Event_covs_meta <- Event_covs %>% extract_metadata() 
+
+## Create column metadata list
+Cols_metadata_l <- list(Bird_abu = Bird_abu_meta, Site_covs = Site_covs_meta, Event_covs = Event_covs_meta, Taxonomy = Taxonomy_meta2, Functional_traits = Fn_traits_meta2)
+
+# >Export  ------------------------------------------------------
+
+# Save the metadata list for each Excel included in repository
+saveRDS(Cols_metadata_l, file = "Data_paper/Rdata/Cols_metadata_l.rds") 
+
+# Export Excel but write the definitions manually in Excel
+if(FALSE){
+  Bird_abu_meta %>% 
+    select(Field_name) %>% 
+    mutate(Definition = NA) %>% 
+    data.frame() %>%
+    xlsx::write.xlsx(file = "Data_paper/Column_definitions.xlsx", showNA = FALSE,
+                     row.names = FALSE, sheetName = "Bird_abundances.csv")
+}
 
 # EXTRAS ------------------------------------------------------------------
 # >Spatial maps -----------------------------------------------------------
